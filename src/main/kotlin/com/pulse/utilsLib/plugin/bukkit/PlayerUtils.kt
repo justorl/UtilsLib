b03e2a76958d89bbf9511cd3e3fc96a9d1b4b8f6
justorl <143778077+justorl@users.noreply.github.com>
2025-11-22 01:48:48 +0300

    1.0.1

--- src/main/kotlin/com/pulse/utilsLib/plugin/bukkit/PlayerUtils.kt
+++ src/main/kotlin/com/pulse/utilsLib/plugin/bukkit/PlayerUtils.kt
@@ -1,6 +1,7 @@
 package com.pulse.utilsLib.plugin.bukkit
 
 import org.bukkit.Bukkit
+import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.attribute.Attribute
 import org.bukkit.entity.Player
@@ -8,6 +9,16 @@ import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.SkullMeta
 
 val allPlayers get() = Bukkit.getOnlinePlayers()
+
+fun Player.pull(target: Location, strength: Double = 1.0) {
+    val from = this.location.toVector()
+    val to = target.toVector()
+
+    val direction = to.subtract(from).normalize()
+    val vel = direction.multiply(strength)
+
+    velocity = vel
+}
 
 fun Player.getPlayerHead(): ItemStack {
     val head = ItemStack(Material.PLAYER_HEAD)
